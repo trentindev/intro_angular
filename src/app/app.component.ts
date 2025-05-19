d866419ea2ee31f70783fe2b6c191ef51ee1dd05
Laurent Dislaire <siladire@free.fr>
2025-05-19 14:13:23 +0200

    Initialisation

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-root",
+  imports: [],
+  template: ` <h1>Popol</h1> `,
+  styleUrls: ["./app.component.css"],
+})
+export class AppComponent {
+  title = "default";
+}
