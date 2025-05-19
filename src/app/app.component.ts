27ba268647c84a0b73d6d2919c16e390d9ebf252
Laurent Dislaire <siladire@free.fr>
2025-05-19 14:43:30 +0200

    create home.component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,24 @@
 import { Component } from "@angular/core";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: "app-root",
-  imports: [],
-  template: ` <h1>Popol</h1> `,
+  imports: [HomeComponent],
+  template: `
+    <main>
+      <header class="brand-name">
+        <img
+          class="brand-logo"
+          src="/assets/logo.svg"
+          alt="logo"
+          aria-hidden="true"
+        />
+      </header>
+      <section class="content">
+        <app-home />
+      </section>
+    </main>
+  `,
   styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
