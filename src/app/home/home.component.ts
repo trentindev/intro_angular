c64ec320a95e6588840a88b7f913d5936b05fe6f
Laurent Dislaire <siladire@free.fr>
2025-05-20 09:32:28 +0200

    Part 3

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,9 +1,10 @@
+import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
-
+import { HousingLocation } from "../housinglocation";
 @Component({
   selector: "app-home",
-  imports: [HousingLocationComponent],
+  imports: [CommonModule, HousingLocationComponent],
   template: `
     <section>
       <form>
@@ -17,4 +18,16 @@ import { HousingLocationComponent } from "../housing-location/housing-location.c
   `,
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly baseUrl = "https://angular.dev/assets/images/tutorials/common";
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: "Test Home",
+    city: "Test city",
+    state: "ST",
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -1,15 +1,12 @@
-import { Component } from '@angular/core';
-
+import { CommonModule } from "@angular/common";
+import { Component, Input } from "@angular/core";
+import { HousingLocation } from "../housinglocation";
 @Component({
-  selector: 'app-housing-location',
-  imports: [],
-  template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
-  styles: ``
+  selector: "app-housing-location",
+  imports: [CommonModule],
+  template: ` <p>housing-location works!</p> `,
+  styleUrls: ["./housing-location.component.css"],
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation;
 }
