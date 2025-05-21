1ffb7244209f65855ea120475895b680ac32cf3c
Laurent Dislaire <siladire@free.fr>
2025-05-21 09:18:17 +0200

    part_7

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -4,7 +4,20 @@ import { HousingLocation } from "../housinglocation";
 @Component({
   selector: "app-housing-location",
   imports: [CommonModule],
-  template: ` <p>housing-location works!</p> `,
+  template: `
+    <section class="listing">
+      <img
+        class="listing-photo"
+        [src]="housingLocation.photo"
+        alt="Exterior photo of {{ housingLocation.name }}"
+        crossorigin
+      />
+      <h2 class="listing-heading">{{ housingLocation.name }}</h2>
+      <p class="listing-location">
+        {{ housingLocation.city }}, {{ housingLocation.state }}
+      </p>
+    </section>
+  `,
   styleUrls: ["./housing-location.component.css"],
 })
 export class HousingLocationComponent {
