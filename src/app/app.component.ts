1f318c20457ea391626cd0bb815eaf560f7a43cf
Laurent Dislaire <siladire@free.fr>
2025-05-21 13:02:18 +0200

    edit part_11

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,8 @@
 import { Component } from "@angular/core";
 import { RouterModule } from "@angular/router";
-import { HomeComponent } from "./home/home.component";
 @Component({
   selector: "app-root",
-  imports: [HomeComponent, RouterModule],
+  imports: [RouterModule],
   template: `
     <main>
       <a [routerLink]="['/']">
